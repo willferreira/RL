dcd46a1c7d38668b6e6696e9ad43bbb0c453d94d
William Ferreira <williamferreira@Williams-MacBook-Pro.local>
2015-03-17 15:45:25 +0000

    Changed MC surface plot axis

--- question2.py
+++ question2.py
@@ -182,7 +182,7 @@ if __name__ == '__main__':
         df = pd.DataFrame(v)
         df.index = range(1, NUMBER_OF_CARDS+1)
         df.columns = range(1, MAX_POINTS+1)
-        df = df.ix[:, range(12, 22)]
+        df = df.ix[:, range(1, 22)]
 
         from mpl_toolkits.mplot3d import axes3d
         import matplotlib.pyplot as plt
@@ -191,11 +191,11 @@ if __name__ == '__main__':
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        X, Y = np.meshgrid(range(1, 11), range(12, 22))
+        X, Y = np.meshgrid(range(1, 11), range(1, 22))
         ax.plot_surface (X, Y, df.values.T, cmap=cm.jet, rstride=1, cstride=1)
         plt.xlabel('Dealer Showing')
         plt.ylabel('Player Sum')
-        plt.yticks(np.arange(12, 22, 2))
+        plt.yticks(np.arange(1, 22, 5))
         plt.show()
 
     if args.plot is not None:
